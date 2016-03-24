7beacb6fd1a206129376bb6cb19875c0d77c60e3
Ted Pattison <ted@tedpattison.net>
2016-03-23 19:42:10 -0700

    1.0 Updates

--- Setup/Scripts/Script01_InstallActiveDiretory.ps1
+++ Setup/Scripts/Script01_InstallActiveDiretory.ps1
@@ -1,24 +1,78 @@
+function Update-HostFile{
 
-$domainName = "wingtip.com"
-$domainNetbiosName = "WINGTIP"
-$administratorPassword = "Password1"
+    Write-Host "Updating HOSTS file on local machine..."
 
 
-# Install the AD-Domain-Services role on the domain controller AD2012R2
-Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools    
+    $hostsFilePath = "c:\Windows\System32\Drivers\etc\hosts"
+    Clear-Content -Path $hostsFilePath 
 
-# Establish a new forest
-Install-ADDSForest `
-    -DomainName $domainName  `
-    -DomainNetbiosName $domainNetbiosName `
-    -SafeModeAdministratorPassword (ConvertTo-SecureString $administratorPassword -AsPlainText -Force) `
-    -CreateDnsDelegation:$false `
-    -DatabasePath "C:\Windows\NTDS" `
-    -DomainMode "Win2012R2" `
-    -ForestMode "Win2012R2" `
-    -InstallDns:$true `
-    -LogPath "C:\Windows\NTDS" `
-    -NoRebootOnCompletion:$false `
-    -SysvolPath "C:\Windows\SYSVOL" `
-    -Force:$true `
-    -Confirm:$false
+    $dnsNames = "wingtipserver", `
+                "wingtipserver.wingtip.com", 
+                "wingtip.com", 
+                "my.wingtip.com", 
+                "intranet.wingtip.com", 
+                "dev.wingtip.com", 
+                "www.wingtip.com", 
+                "search.wingtip.com", 
+                "research.wingtip.com", 
+                "disco.wingtip.com", 
+                "bi.wingtip.com"
+
+    foreach ($dnsName in $dnsNames){
+        Add-Content -Path $hostsFilePath -Value "127.0.0.1`t$dnsName"
+    }
+ 
+    Write-Host "HOSTS file updated on local machine"
+    Write-Host 
+}
+
+function Install-ActiveDirectory{
+
+    Write-Host "Installing Active Directory on local machine..."
+
+    Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools    
+
+    Write-Host "Active Directory installed on local machine"
+    Write-Host 
+}
+
+function Create-ActiveDirectoryDomain{
+
+
+    Write-Host "Creating new Active Directory domain wingtip.com..."
+
+    $domainName = "wingtip.com"
+    $domainNetbiosName = "WINGTIP"
+    $administratorPassword = "Password1"
+
+    # Establish a new forest
+    Install-ADDSForest `
+        -DomainName $domainName  `
+        -DomainNetbiosName $domainNetbiosName `
+        -SafeModeAdministratorPassword (ConvertTo-SecureString $administratorPassword -AsPlainText -Force) `
+        -CreateDnsDelegation:$false `
+        -DatabasePath "C:\Windows\NTDS" `
+        -DomainMode "Win2012R2" `
+        -ForestMode "Win2012R2" `
+        -InstallDns:$true `
+        -LogPath "C:\Windows\NTDS" `
+        -NoRebootOnCompletion:$false `
+        -SysvolPath "C:\Windows\SYSVOL" `
+        -Force:$true `
+        -Confirm:$false
+
+
+    Write-Host "Active Directory domain wingtip.com has been created."
+    Write-Host 
+}
+
+cls
+
+Update-HostFile
+
+Install-ActiveDirectory
+
+Create-ActiveDirectoryDomain
+
+Write-Host "This script has completed."
+Write-Host 
